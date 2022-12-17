4d39ec3c9bf9e0476af3ae57f56159238007545d
peterjxl <peterjxl@qq.com>
2022-12-17 16:33:59 +0800

    演示Double的ToRawLongBits方法

---
+++ 01.JavaSE/02.DataType/TestDoubleToRawLongBits.java
@@ -0,0 +1,9 @@
+public class TestDoubleToRawLongBits {
+  public static void main(String[] args) {
+    double d  = 1;
+    System.out.println(Double.doubleToRawLongBits(d));
+
+    double d2 = 0.1;
+    System.out.println(Double.doubleToRawLongBits(d2));
+  }
+}
