543900ce9f2903330d83bc90d45968fc4164cd6f
peterjxl <peterjxl@qq.com>
2022-12-20 22:40:16 +0800

    测试Boolean和String

---
+++ 01.JavaSE/05.OOP/15.character/TestStringLength.java
@@ -0,0 +1,11 @@
+public class TestStringLength {
+  public static void main(String[] args) {
+    String str = "A";
+    System.out.println(str.length());
+
+    String str2 = "😂";
+    System.out.println(str2.length());
+    System.out.println(str2.codePointCount(0,str2.length())); //输出1
+    
+  }
+}
