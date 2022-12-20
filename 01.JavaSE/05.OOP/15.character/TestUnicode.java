136d5480166d26282a85152d9f86c829abb345ee
peterjxl <peterjxl@qq.com>
2022-12-20 20:09:04 +0800

    Unicode相关代码

--- 01.JavaSE/05.OOP/15.character/TestUnicode.java
+++ 01.JavaSE/05.OOP/15.character/TestUnicode.java
@@ -2,5 +2,8 @@ public class TestUnicode {
   public static void main(String[] args) {
     String str = "严";
     System.out.println(str.codePointAt(0));
+
+    str = "\u4E25";
+    System.out.println(str);
   }
 }
