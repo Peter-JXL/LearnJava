f4318b2d8aa82ac185ae306b117348bda18f7062
peterjxl <peterjxl@qq.com>
2022-12-26 11:33:29 +0800

    测试枚举类

---
+++ 01.JavaSE/05.OOP/40.Time/TestDayOfWeek.java
@@ -0,0 +1,9 @@
+import java.time.DayOfWeek;
+
+public class TestDayOfWeek {
+  public static void main(String[] args) {
+    DayOfWeek d = DayOfWeek.of(4);
+    System.out.println(d.name());  //THURSDAY
+    System.out.println(d.getValue());  //4
+  }
+}
