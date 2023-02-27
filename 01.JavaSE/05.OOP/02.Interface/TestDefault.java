c7b2e1732c9d76377bb49deeca59c11a9dd2fe16
peterjxl <peterjxl@qq.com>
2023-02-27 09:37:49 +0800

    测试静态字段

---
+++ 01.JavaSE/05.OOP/02.Interface/TestDefault.java
@@ -0,0 +1,18 @@
+public class TestDefault {
+  public static void main(String[] args) {
+    InterfaceWithDefault stu = new Student("Peter JXL");  
+    stu.run();
+  }
+}
+
+class Student implements InterfaceWithDefault{
+  private String name;
+
+  public Student(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+}
---
+++ 01.JavaSE/05.OOP/04.static/StaticInterview1.java
@@ -0,0 +1,13 @@
+public class StaticInterview1 {
+
+  static{
+    System.out.println("test static 1");
+  }
+  public static void main(String[] args) {
+    
+  }
+
+  static{
+    System.out.println("test static 2");
+  }
+}
